baeb10bf2da282d4a5e4ba189d0ea00d6317fe0c
AbdullahShwareb <shwareba700@gmail.com>
2025-11-01 19:14:41 +0200

    uploud

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,5 +1,5 @@
 import styles from './Navbar.module.css';
-import { Link } from 'react-router-dom'; // ✅ مهم جداً
+import { Link } from 'react-router-dom'; 
 
 export default function Navbar() {
   return (
